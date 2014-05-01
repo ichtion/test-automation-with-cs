e9ee003924eefe1756f13ac010ef35f9e8294132
kir <remigiusz_dudek@tlen.pl>
2014-05-01 12:06:49 +0200

    refactored PictureRetrieverTest

--- src/test/java/codeSprinters/refactoring/PictureRetrieverTest.java
+++ src/test/java/codeSprinters/refactoring/PictureRetrieverTest.java
@@ -3,59 +3,58 @@ package codeSprinters.refactoring;
 import codeSprinters.refactoring.dao.PictureDao;
 import codeSprinters.refactoring.domain.Picture;
 import codeSprinters.refactoring.domain.User;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
-
-import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 
 public class PictureRetrieverTest {
 
     private PictureRetriever pictureRetriever = new PictureRetriever();
 
     @Test(expected = NoLoggedUserException.class)
-    public void testGetPicturesInvalid() throws Exception {
+    public void test1() throws Exception {
         User user1 = new User();
         pictureRetriever.getPicturesFor(user1, null);
     }
 
     @Test
-    public void testGetPicturesValid() throws NoLoggedUserException {
+    public void test2() throws NoLoggedUserException {
         Picture picture = new Picture();
         User user1 = new User();
         User user2 = new User();
         user2.addFriend(user1);
-        PictureDao.addPicturesForUser(user1, asList(picture));
+        PictureDao.addPicturesForUser(user1, Arrays.asList(picture));
 
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
 
-        assertThat(list, containsInAnyOrder(picture));
+        Assert.assertThat(list, Matchers.containsInAnyOrder(picture));
     }
 
     @Test
-    public void testGetPictureInValid_2() throws NoLoggedUserException {
+    public void test3() throws NoLoggedUserException {
         Picture picture = new Picture();
         User user1 = new User();
         User user2 = new User();
 
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
 
-        assertThat(list, is(nullValue()));
+        Assert.assertThat(list, Matchers.is(Matchers.nullValue()));
     }
 
     @Test
-    public void testGetPictureInValid_3() throws NoLoggedUserException {
+    public void test4() throws NoLoggedUserException {
         Picture picture = new Picture();
         User user1 = new User();
         User user2 = new User();
         User user3 = new User();
         user2.addFriend(user3);
-        PictureDao.addPicturesForUser(user1, asList(picture));
+        PictureDao.addPicturesForUser(user1, Arrays.asList(picture));
 
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
 
-        assertThat(list, is(nullValue()));
+        Assert.assertThat(list, Matchers.is(Matchers.nullValue()));
     }
 }
